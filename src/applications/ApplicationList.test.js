c6e7ba4931e86162b6594d89688d7589c7d8167b
Domenic <dsjobs808@gmail.com>
2023-07-24 21:50:08 -0700

    Testing push

--- src/applications/ApplicationList.test.js
+++ src/applications/ApplicationList.test.js
@@ -26,16 +26,17 @@ const testUser2 = {
 }
 
 /**
- * TODO: 
+ * Testing push
+ * TODO:
  * - write test that checks user applications show
- *  - test negative if no applications make sure test for "No applications sent so far" 
- * 
+ *  - test negative if no applications make sure test for "No applications sent so far"
+ *
  * - mock a button press that when user clicks unapply button on page it chagnes to apply
- *  
+ *
  * - integration test that test that when unapply is clicked
  *  - check that unappy switches to -> apply
  *  - check that when user refreshes page that the application is gone
- * 
+ *
  */
 
 test('test application page', () => {
