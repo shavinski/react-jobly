c3bfcce807d9c29d9345f1d20896cead64293902
Jakob <shavinski.jakob@gmail.com>
2024-02-21 12:38:45 -0800

    Figure out what error is occuring with API

--- src/joblyApi.js
+++ src/joblyApi.js
@@ -33,7 +33,7 @@ class JoblyApi {
       return (await axios({ url, method, data, params, headers })).data;
     } catch (err) {
       console.error("API Error:", err.response);
-      let message = err.response.data.error.message;
+      let message = err.response.data.error.message || "Unknown error";
       throw Array.isArray(message) ? message : [message];
     }
   }
